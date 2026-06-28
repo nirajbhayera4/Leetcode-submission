af40ed75e1c9e3bc9090c147d95843cf5e9ce56e
NIRAJ <nirajbhayera4@gmail.com>
2026-06-28 11:14:17 +0530

    Time: 2 ms (78.68%), Space: 7.7 MB (88.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,33 @@
 
 class Solution {
 public:
-int dp[31];
+    /*Solution()
+    {
+        memset(dp,-1,sizeof(dp));
+    }
+    */
 
-Solution()
-{
-    memset(dp,-1,sizeof(dp));
-}
+    /*int solve(int n, vector<int>& t) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (t[n] != -1)
+            return t[n];
+        return t[n] = fib(n - 1) + fib(n - 2);
+    }
+    */
     int fib(int n) {
-        if(n==0)return 0;
-        if(n==1)return 1;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=fib(n-1) + fib(n-2);
+        int t[31];
+        memset(t, -1, sizeof(t));
+        function<int(int)> solve = [&](int n) {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            if (t[n] != -1)
+                return t[n];
+            return t[n] = solve(n - 1) + solve(n - 2);
+        }; return solve(n);
     }
 };
