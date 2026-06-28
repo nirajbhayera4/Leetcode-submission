a5a9c454fb7a3d033276686a8f0091047085054f
NIRAJ <nirajbhayera4@gmail.com>
2026-06-28 11:07:13 +0530

    Time: 0 ms (100%), Space: 7.6 MB (98.77%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+int dp[31];
+
+Solution()
+{
+    memset(dp,-1,sizeof(dp));
+}
+    int fib(int n) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=fib(n-1) + fib(n-2);
+    }
+};
