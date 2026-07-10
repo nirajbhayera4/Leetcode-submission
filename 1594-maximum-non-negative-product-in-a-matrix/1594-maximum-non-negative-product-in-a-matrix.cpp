f5aef77e976e5447b42011b21da1874bf493ebd2
NIRAJ <nirajbhayera4@gmail.com>
2026-07-10 13:20:10 +0530

    Time: 1 ms (40.95%), Space: 14.1 MB (55.36%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+typedef long long ll;
+int MOD=1e9 +7;
+
+/*
     int m, n;
     int MOD = 1e9 + 7;
 
@@ -31,13 +35,41 @@ public:
         }
         return t[i][j] = {maxval, minval};
     }
+    */
 
     int maxProductPath(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        t = vector<vector<pair<ll, ll>>>(
+        int m = grid.size();
+        int n = grid[0].size();
+        /*t = vector<vector<pair<ll, ll>>>(
             m, vector<pair<ll, ll>>(n, {LLONG_MIN, LLONG_MAX}));
         auto [maxprod, minprod] = solve(0, 0, grid);
         return maxprod < 0 ? -1 : maxprod % MOD;
+        */
+        vector<vector<pair<ll, ll>>> t(m,vector<pair<ll,ll>>(n));
+        t[0][0]={grid[0][0], grid[0][0]};
+
+        // column
+        for(int j=1;j<n;j++){
+            t[0][j].first=t[0][j-1].first * grid[0][j];
+            t[0][j].second=t[0][j-1].second * grid[0][j];
+        }
+        //row
+        for(int i=1;i<m;i++){
+            t[i][0].first=t[i-1][0].first * grid[i][0];
+            t[i][0].second=t[i-1][0].second * grid[i][0];
+
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                ll upmax=t[i-1][j].first;
+                ll upmin=t[i-1][j].second;
+                ll leftmax=t[i][j-1].first;
+                ll leftmin=t[i][j-1].second;
+                t[i][j].first=max({1LL *upmax * grid[i][j],1LL * upmin * grid[i][j], 1LL *leftmax* grid[i][j], 1LL *leftmin * grid[i][j]});
+                t[i][j].second=min({1LL *upmax * grid[i][j],1LL * upmin * grid[i][j], 1LL *leftmax* grid[i][j], 1LL *leftmin * grid[i][j]});
+            }
+        }
+        auto [maxprod, minprod] = t[m-1][n-1];
+        return maxprod <0 ? -1:maxprod %MOD;
     }
 };
