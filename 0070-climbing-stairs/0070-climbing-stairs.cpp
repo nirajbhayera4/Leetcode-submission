743de1c0c93fb8ac17c13451dbb38a1b7bf743ab
NIRAJ <nirajbhayera4@gmail.com>
2026-07-23 20:32:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
-        
-
-        /*if(n==1 || n==2 || n==3){
-            return n;
-        }
-
+        if(n==1)return 1;
+        if(n==2)return 2;
         vector<int> t(n+1);
-
-        t[0]=0;
+        
         t[1]=1;
         t[2]=2;
         for(int i=3;i<=n;i++){
-            t[i]=t[i-1] +t[i-2];
+            t[i]=t[i-1] + t[i-2];
+
         }
         return t[n];
-        */
     }
 };
