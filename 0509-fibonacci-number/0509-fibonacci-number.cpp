8d0eebcc9bb707b51adc178793efc0da3117b2ef
NIRAJ <nirajbhayera4@gmail.com>
2026-07-23 20:17:04 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,49 +1,15 @@
-
 class Solution {
 public:
-    /*Solution()
-    {
-        memset(dp,-1,sizeof(dp));
-    }
-    */
-
-    /*int solve(int n, vector<int>& t) {
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-        if (t[n] != -1)
-            return t[n];
-        return t[n] = fib(n - 1) + fib(n - 2);
-    }
-    */
-    int fib(int n) {
-        /*int t[31];
-        memset(t, -1, sizeof(t));
-        function<int(int)> solve = [&](int n) {
-            if (n == 0)
-                return 0;
-            if (n == 1)
-                return 1;
-            if (t[n] != -1)
-                return t[n];
-            return t[n] = solve(n - 1) + solve(n - 2);
-        }; return solve(n);*/
-
-        if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
-
-        vector<int> dp(n + 1, -1);
-
-        // state define krenge
-        dp[0]=0;
-        dp[1]=1;
-
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1] + dp[i-2];
-        }
+int solve(int n,vector<int> &dp){
+    if(n==0)return 0;
+    if(n==1)return 1;
+    if(dp[n]!=-1){
         return dp[n];
+    }
+    return dp[n]=solve(n-1,dp) + solve(n-2,dp);
+}
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);;
     }
 };
