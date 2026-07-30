b1200976a54ac91110a19ad28b6cf84df9476542
NIRAJ <nirajbhayera4@gmail.com>
2026-07-30 08:50:26 +0530

    Time: 81 ms (63.51%), Space: 44.4 MB (68.72%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-unordered_map<int, vector<pair<int, int>>>adj;
-        //adj list banayenge
-        for(auto &vec : times){
+        unordered_map<int, vector<pair<int,int>>>adj;
+        for(auto  &vec : times){
             int u=vec[0];
             int v=vec[1];
             int w=vec[2];
-
             adj[u].push_back({v,w});
         }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-        vector<int> result(n+1,INT_MAX);
+        priority_queue<pair<int, int>,vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        vector<int> result(n+1, INT_MAX);
         result[k]=0;
         pq.push({0,k});
         while(!pq.empty()){
@@ -22,16 +20,16 @@ unordered_map<int, vector<pair<int, int>>>adj;
             for(auto &vec : adj[node]){
                 int adjn=vec.first;
                 int wt=vec.second;
-
                 if(d+wt < result[adjn]){
-                    result[adjn]=d+wt;
-                    pq.push({d+wt,adjn});
+                    result[adjn] =d+wt;
+                    pq.push({d+wt, adjn});
+
                 }
             }
         }
         int ans=0;
         for(int i=1;i<=n;i++){
-            ans=max(ans, result[i]);
+            ans=max(ans,result[i]);
         }
         return ans==INT_MAX? -1 : ans;
     }
