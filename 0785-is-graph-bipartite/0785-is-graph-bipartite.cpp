7034dc99db207d33c55ee21d570a7ed9ded175d3
NIRAJ <nirajbhayera4@gmail.com>
2026-07-31 21:03:48 +0530

    Time: 0 ms (100%), Space: 17.7 MB (14.09%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+
+//bfs se krenge 
+bool bipartite(vector<vector<int>>& graph, int u, vector<int>& color, int currcolor){
+    queue<int> q;
+    q.push(u);
+    color[u]=currcolor;
+    while(!q.empty()){
+        int curr=q.front();
+        q.pop();
+        for(auto &vec : graph[curr]){
+            if(color[vec]==color[curr])return false;
+            if(color[vec]==-1){
+               
+                // bfs hai to wapis queue me push kr denge 
+                color[vec] = 1 - color[curr];// wapis assign kr denge 
+                q.push(vec);
+            }
+        }
+
+    }
+    return true;
+}
+/*
 bool bipartite(vector<vector<int>>& graph, int u, vector<int> &color, int currcolor){
     color[u]=currcolor;
     for(auto &v : graph[u]){
@@ -13,6 +36,7 @@ bool bipartite(vector<vector<int>>& graph, int u, vector<int> &color, int currco
     }
     return true;
 }
+*/
 
     bool isBipartite(vector<vector<int>>& graph) {
          int n=graph.size();
