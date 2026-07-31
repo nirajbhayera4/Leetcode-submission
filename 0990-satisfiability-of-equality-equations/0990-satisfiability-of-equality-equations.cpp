e7eef674c3d9a5d33cb158055188ee4ddd346d39
NIRAJ <nirajbhayera4@gmail.com>
2026-07-31 22:01:42 +0530

    Time: 0 ms (100%), Space: 15 MB (56.87%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -1,67 +1,54 @@
 class Solution {
 public:
-vector<int>parent;
-vector<int>rank;
-int find(int i){
-    if(parent[i]==i)return i;
-    return parent[i]=find(parent[i]);
-}
-
-void Union(int x, int y){
-    int xp=find(x);
-    int yp=find(y);
-
-    if(xp==yp)return ;
-
-    if(rank[xp]> rank[yp]){
-        parent[yp]=xp;
-
-    }
-    else if(rank[xp] < rank[yp]){
-        parent[xp]=yp;
-
-    }
-    else {
-        parent[xp]=yp;
-        rank[yp]++;
+    vector<int> parent;
+    vector<int> rank;
+    int find(int i){
+        if(parent[i]==i)return i;
+        return parent[i]=find(parent[i]);
     }
 
-
-}
-
+    void Union(int x, int y){
+        int xp=find(x);
+        int yp=find(y);
+        if(xp==yp)return ;
+        if(rank[xp] > rank[yp]){
+            parent[yp]=xp;
+        }
+        else if(rank[xp]<rank[yp]){
+            parent[xp]=yp;
+        }
+        else{
+            parent[xp]=yp;
+            rank[yp]++;
+        }
+    }
     bool equationsPossible(vector<string>& equations) {
         parent.resize(26);
         rank.resize(26);
 
-        //starting me sb apne apne parent honge 
-        for(int i=0;i<26;i++){
-            parent[i]=i;
+        for (int i = 0; i < 26; i++) {
+            parent[i] = i;
         }
 
-        // case1 : "=" ke liye
-
-        for(string &s :equations){
-            if(s[1]=='='){
-                Union(s[0]-'a', s[3]-'a');
-
+        for (string& s : equations) {
+            if (s[1] == '=') {
+                Union(s[0] - 'a', s[3] - 'a');
             }
         }
 
-        //case2 : "!" ke liye 
-        for(string &s : equations){
+        for(string &s: equations){
             if(s[1]=='!'){
-                char first=s[0];
-                char second=s[3];
+                char first=s[0]; //a
+                char second=s[3];//b
 
-                int fp=find(first -'a');
-                int sp=find(second-'a');
+                int fp=find(first- 'a');
+                int sp=find(second- 'a');
 
-                if(fp==sp){
-                    return false;
-                }
+
+                if(fp==sp)return false;
+
             }
         }
         return true;
-
     }
 };
