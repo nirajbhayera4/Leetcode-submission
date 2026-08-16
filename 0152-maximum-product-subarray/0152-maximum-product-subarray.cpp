4b72f54fd9f17d938a36d0436872dde1e2f8c864
NIRAJ <nirajbhayera4@gmail.com>
2026-08-16 11:22:29 +0530

    Time: 2 ms (15.78%), Space: 17.6 MB (99.51%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
-int solve(vector<int>& nums){
+int solve(vector<int> &nums){
     int n=nums.size();
     if(n==0)return 0;
-
     int curr_max=nums[0];
     int curr_min=nums[0];
     int res=nums[0];
-
-    for(int j=1;j<n;j++){
+    for(int i=1;i<n;i++){
         int temp=curr_max;
+        curr_max=max({nums[i],temp * nums[i],curr_min * nums[i]});
+        curr_min=min({nums[i],temp * nums[i],curr_min * nums[i]});
+        res=max(res, curr_max);
+        
 
-        curr_max=max({nums[j],temp * nums[j], curr_min *nums[j]});
-        curr_min=min({nums[j],temp * nums[j], curr_min *nums[j]});
-        res=max(curr_max,res);
+
     }
     return res;
+
 }
     int maxProduct(vector<int>& nums) {
-        
+       
         return solve(nums);
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
