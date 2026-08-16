85059a7def535a723104c6200bcb5a57f0bc8143
NIRAJ <nirajbhayera4@gmail.com>
2026-08-16 14:53:58 +0530

    Time: 451 ms (25.7%), Space: 41.5 MB (36.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,53 +1,39 @@
 class Solution {
 public:
-   /* int n;
-    int t[2501][2501];
+int n;
+int t[2501][2501];
+int solve(vector<int> &nums, int idx, int p){
+    if(idx >=n){
+        return 0;
 
-    int solve(vector<int>& nums, int i, int P) {
-        if (i >= n) {
-            // out od bound check krenge
-            return 0;
-        }
-
-        if (P != -1 && t[i][P] != -1) {
-            return t[i][P];
-        }
-        int take = 0;
-
-        if (P == -1 || nums[P] < nums[i]) {
-            take = 1 + solve(nums, i + 1, i);
-
-            // warna skip krdo
-        }
-        int skip = solve(nums, i + 1, P);
-        if (P != -1) {
-            t[i][P] = max(take, skip);
-        }
-
-        return max(take, skip);
     }
-    */
+    if(p !=-1 && t[idx][p]!=-1){
+        return t[idx][p];
+    }
+
+    // take condition 
+    int take=0;
+    if(p ==-1 ||  nums[p] < nums[idx]){
+        take=1 + solve(nums, idx+1, idx);
+    }
+
+    // skip ccondition 
+    // if take nahi hoga to obv skiphi krega 
+    int skip= solve(nums , idx +1 , p);
+    if(p!=-1){
+         t[idx][p]=max(take, skip);
+    }
+    return max(take, skip);
+
+
+}
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-
-        // initialize dp
-        //memset(t, -1, sizeof(t));
-        //return solve(nums, 0, -1);
-        vector<int> t(n,1);
-        int maxLIS=1;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]< nums[i]){
-                    t[i]=max(t[i],t[j]+1 );
-                    maxLIS=max(maxLIS,t[i]);
-                }
-            }
-            
-        }
-        return maxLIS;
-
-
-    
+        n=nums.size();
+        memset(t, -1, sizeof(t));
+        return solve(nums, 0,-1);
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
