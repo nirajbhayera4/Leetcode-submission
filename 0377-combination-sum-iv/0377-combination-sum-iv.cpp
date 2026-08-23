23687df99e14177292dbc8900e717b7068970174
NIRAJ <nirajbhayera4@gmail.com>
2026-08-23 11:15:04 +0530

    Time: 0 ms (100%), Space: 9.3 MB (24.41%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+int backtrack(vector<int> &nums, int target,vector<int> &dp){
+    // valid combo 
+
+    if(target==0){
+        return 1;
+    }
+
+    // invalid combo 
+    if(target <0){
+        return 0;
+    }
+    if(dp[target]!=-1){
+        return dp[target];
+    }
+    int count=0;
+
+
+    for(int i=0;i<nums.size();i++){
+        count+=backtrack(nums, target -nums[i], dp);
+
+    }
+    return dp[target]=count;
+}
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, -1);
+        return backtrack(nums, target,dp);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
