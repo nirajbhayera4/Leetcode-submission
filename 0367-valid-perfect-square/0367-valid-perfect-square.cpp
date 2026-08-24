4434578b3ed6bbc14444828879a7910f7215f103
NIRAJ <nirajbhayera4@gmail.com>
2026-08-24 18:42:49 +0530

    Time: 0 ms (100%), Space: 7.7 MB (53.49%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if(num< 2)return true ;
+        long low=1,high=num;
+        while(low<=high){
+            long mid=low + (high -low)/2;
+
+            if(mid* mid==num){
+                return true;
+            }
+
+            else if(mid * mid >num){
+                high=mid -1;
+
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
