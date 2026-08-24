a12a6604983cef64ed4ad9bf3c91760d9b406bec
NIRAJ <nirajbhayera4@gmail.com>
2026-08-24 19:03:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (60.19%) - LeetHub

---
+++ 3663-find-the-least-frequent-digit/3663-find-the-least-frequent-digit.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int getLeastFrequentDigit(int n) {
+        int hash[10]={0};
+
+        int temp=n,freq=10,ans=0;
+
+        while(temp >0){
+            hash[temp % 10]++;
+            temp/=10;
+        }
+
+        for(int i=9;i>=0;i--){
+            if(hash[i]==0)continue;
+            else{
+                if(hash[i]<=freq){
+                    freq=hash[i];
+                    ans=i;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
