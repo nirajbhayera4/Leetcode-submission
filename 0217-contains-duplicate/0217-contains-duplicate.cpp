a06b761b6cb976deb01794dcc81a9da8f048ae9a
NIRAJ <nirajbhayera4@gmail.com>
2026-08-30 23:06:58 +0530

    Time: 71 ms (61.01%), Space: 113.3 MB (22.83%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        for(auto &it  : freq)
+        {
+            if(it.second >=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
