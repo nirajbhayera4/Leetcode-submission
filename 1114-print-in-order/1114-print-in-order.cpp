61f2156caa4f00d2e19ccdc7bb4cd412383bb5b9
NIRAJ <nirajbhayera4@gmail.com>
2026-09-23 23:15:26 +0530

    Time: 13 ms (20.32%), Space: 9.5 MB (79.28%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+class Foo {
+private:
+    std::mutex mtx;
+    std::condition_variable cv;
+    int state = 0; // 0: first, 1: second, 2: third
+
+public:
+    Foo() {}
+
+    void first(std::function<void()> printFirst) {
+        std::unique_lock<std::mutex> lock(mtx);
+        printFirst();
+        state = 1;
+        cv.notify_all(); // Notify second()
+    }
+
+    void second(std::function<void()> printSecond) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]{ return state == 1; }); // Wait for first()
+        printSecond();
+        state = 2;
+        cv.notify_all(); // Notify third()
+    }
+
+    void third(std::function<void()> printThird) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]{ return state == 2; }); // Wait for second()
+        printThird();
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
